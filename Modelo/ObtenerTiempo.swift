6fd78005f730d3dfa554bcf9cdd4e54ceb76d24a
gabrielCSSM <144927622+gabrielCSSM@users.noreply.github.com>
2024-06-09 19:29:04 +0200

    Update ObtenerTiempo.swift

--- Modelo/ObtenerTiempo.swift
+++ Modelo/ObtenerTiempo.swift
@@ -14,93 +14,77 @@
 import OpenMeteoSdk
 import Foundation
 
-func obtenerURL() -> (String, String) {
-    var latitudes: String = "latitude="
-    var longitudes: String = "longitude="
+/// Make sure the URL contains `&format=flatbuffers`
 
-    for(anyCiudad) in obtenerCiudades() {
-        latitudes = latitudes.appending(String(anyCiudad.coordLat) + ",")
-        longitudes = longitudes.appending(String(anyCiudad.coordLong) + ",")
 
+
+
+func obtenerDatosCiudades() {
+    for cadaCiudad in obtenerCiudades() {
+        print(cadaCiudad)
+        print("\n")
+        
+        Task {
+            let myfunc = await llamarAPI(latitude: cadaCiudad.coordLat, longitude: cadaCiudad.coordLong)
+            print(myfunc)
+            print("\n")
+        }
     }
-    
-    latitudes.removeLast()
-    longitudes.removeLast()
-    
-    return (latitudes, longitudes)
 }
 
-/// Make sure the URL contains `&format=flatbuffers`
-let url = URL(string:
-                "https://api.open-meteo.com/v1/forecast?" + obtenerURL().0 + "&"
-                + obtenerURL().1 +
-                "&hourly=temperature_2m&format=flatbuffers")!
-
-func llamarAPI()  {
+func llamarAPI(latitude: Double, longitude: Double) async -> [String] {
     
-    print("entro funcion")
-    
+    let url = URL(string:"https://api.open-meteo.com/v1/forecast?latitude=\(latitude)&longitude=\(longitude)&hourly=temperature_2m&format=flatbuffers")!
     
     do {
         
-        print("entro do")
+        let respuestas = try await WeatherApiResponse.fetch(url: url)
+        let respuesta = respuestas[0];
         
-        Task {
-            print("entro task")
+        /// Attributes for timezone and location
+        let utcOffsetSeconds = respuesta.utcOffsetSeconds
+        let timezone = respuesta.timezone
+        let timezoneAbbreviation = respuesta.timezoneAbbreviation
+        let latitude = respuesta.latitude
+        let longitude = respuesta.longitude
+        
+        let hourly = respuesta.hourly!
+        
+        struct WeatherData {
             
-            let respuestas = try await WeatherApiResponse.fetch(url: url)
+            let hourly: Hourly
             
-            print("salgo respuestas")
-            
-            /// Process first location. Add a for-loop for multiple locations or weather models
-            for respuesta in respuestas {
-                /// Attributes for timezone and location
-                let utcOffsetSeconds = respuesta.utcOffsetSeconds
-                let timezone = respuesta.timezone
-                let timezoneAbbreviation = respuesta.timezoneAbbreviation
-                let latitude = respuesta.latitude
-                let longitude = respuesta.longitude
-                
-                let hourly = respuesta.hourly!
-                
-                
-                struct WeatherData {
-                    let hourly: Hourly
-                    
-                    struct Hourly {
-                        let time: [Date]
-                        let temperature2m: [Float]
-                    }
-                }
-                
-                /// Note: The order of weather variables in the URL query and the `at` indices below need to match!
-                let data = WeatherData(
-                    hourly: .init(
-                        time: hourly.getDateTime(offset: utcOffsetSeconds),
-                        temperature2m: hourly.variables(at: 0)!.values
-                    )
-                )
-                
-                printTiempo(tiempos: data.hourly.temperature2m)
-                
-                /// Timezone `.gmt` is deliberately used.
-                /// By adding `utcOffsetSeconds` before, local-time is inferred
-                let dateFormatter = DateFormatter()
-                dateFormatter.timeZone = .gmt
-                dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
-                
-                
-                for (i, date) in data.hourly.time.enumerated() {
-                    print(dateFormatter.string(from: date))
-                    print(data.hourly.temperature2m[i])
-                }
+            struct Hourly {
+                let time: [Date]
+                let temperature2m: [Float]
             }
         }
         
+        /// Note: The order of weather variables in the URL query and the `at` indices below need to match!
+        let data = WeatherData(
+            hourly: .init(
+                time: hourly.getDateTime(offset: utcOffsetSeconds),
+                temperature2m: hourly.variables(at: 0)!.values
+            )
+        )
+        
+        /// Timezone `.gmt` is deliberately used.
+        /// By adding `utcOffsetSeconds` before, local-time is inferred
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.timeZone = .gmt
+        dateFormatter.dateFormat = "yyyy-MM-dd_HH:mm"
         
         
-    } catch {
-            fatalError("Fallo")
+        var tempArray: [String] = []
+        for (i, date) in data.hourly.time.enumerated() {
+            var mystring = ("\(dateFormatter.string(from: date));\(data.hourly.temperature2m[i])")
+            tempArray.append(mystring)
         }
+        
+        return tempArray
+        
+    }catch {
+        fatalError()
     }
-
+}
